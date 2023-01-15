7f0224ce0701f8212addf68caeb7ed5dfe4ea76f
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-01-15 21:10:54 +0400

    added eslint

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,59 +1,66 @@
 import Image from "next/image";
-import { ShareIcon, HandThumbUpIcon, ChatBubbleLeftIcon } from '@heroicons/react/24/outline';
+import {
+  ShareIcon,
+  HandThumbUpIcon,
+  ChatBubbleLeftIcon,
+} from "@heroicons/react/24/outline";
 
 import { IPost } from "../db/db.types";
 
 export default function Post(props: IPost) {
-    const { name, message, email, timeStamp, image, postImage } = props;
+  const { name, message, email, timeStamp, image, postImage } = props;
 
-    return (
-        <div className="flex flex-col">
-            <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
-                <div className="flex items-center space-x-2">
-                    <img
-                        className="rounded-full"
-                        src={image}
-                        width={40}
-                        height={40}
-                        alt="post" />
-                    <div className="">
-                        <p className="font-medium">{name}</p>
-                        <p className="text-xs text-gray-400">{timeStamp && new Date(timeStamp.toDate()).toLocaleString()}</p>
-                    </div>
-                </div>
-                <p className="pt-4">{message}</p>
-            </div>
+  return (
+    <div className="flex flex-col">
+      <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
+        <div className="flex items-center space-x-2">
+          <Image
+            className="rounded-full"
+            src={image}
+            width={40}
+            height={40}
+            alt="post"
+          />
+          <div className="">
+            <p className="font-medium">{name}</p>
+            <p className="text-xs text-gray-400">
+              {timeStamp && new Date(timeStamp.toDate()).toLocaleString()}
+            </p>
+          </div>
+        </div>
+        <p className="pt-4">{message}</p>
+      </div>
 
-            {/* cover image */}
-            {postImage && (
-                <div className="relative h-56 md:h-96 bg-white">
-                    <Image
-                        src={postImage}
-                        alt="post"
-                        fill
-                        sizes="(max-width: 768px) 100vw,
+      {/* cover image */}
+      {postImage && (
+        <div className="relative h-56 md:h-96 bg-white">
+          <Image
+            src={postImage}
+            alt="post"
+            fill
+            sizes="(max-width: 768px) 100vw,
                         (max-width: 1200px) 50vw,
                         33vw"
-                        style={{ objectFit: 'cover' }}
-                    />
-                </div>
-            )}
-
-            {/* Footer of post */}
-            <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
-                <div className="inputIcon rounded-none rounded-bl-2xl">
-                    <HandThumbUpIcon className="h-4" />
-                    <p className="text-sm sm:text-base">Like</p>
-                </div>
-                <div className="inputIcon rounded-none">
-                    <ChatBubbleLeftIcon className="h-4" />
-                    <p className="text-sm sm:text-base">Comment</p>
-                </div>
-                <div className="inputIcon rounded-none rounded-br-2xl">
-                    <ShareIcon className="h-4" />
-                    <p className="text-sm sm:text-base">Share</p>
-                </div>
-            </div>
+            style={{ objectFit: "cover" }}
+          />
         </div>
-    )
+      )}
+
+      {/* Footer of post */}
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <HandThumbUpIcon className="h-4" />
+          <p className="text-sm sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none">
+          <ChatBubbleLeftIcon className="h-4" />
+          <p className="text-sm sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-sm sm:text-base">Share</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -2,80 +2,103 @@ import { useState, ChangeEvent, FormEvent } from "react";
 
 import { logIn, signInWithGooglePopup } from "../db/user";
 
-import InputField from "./InputField"
+import InputField from "./InputField";
 
 type openRegisterHandler = () => void;
 
-export default function SignInForm({ openRegisterHandler }: { openRegisterHandler: openRegisterHandler }) {
-    const [userCreds, setUserCred] = useState({
-        email: '',
-        password: '',
+export default function SignInForm({
+  openRegisterHandler,
+}: {
+  openRegisterHandler: openRegisterHandler;
+}) {
+  const [userCreds, setUserCred] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserCred({
+      ...userCreds,
+      [e.target.id]: e.target.value,
     });
+  };
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setUserCred({
-            ...userCreds,
-            [e.target.id]: e.target.value
-        });
+  const handleSignIn = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await logIn(userCreds.email, userCreds.password);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const handleSignIn = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const handleGoogleSignIn = async () => {
+    const resp = await signInWithGooglePopup();
+    console.log(resp);
+  };
 
-        try {
-            const res = await logIn(userCreds.email, userCreds.password);
-            console.log(res);
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    const handleGoogleSignIn = async () => {
-        const resp = await signInWithGooglePopup();
-        console.log(resp);
-    }
-
-    return <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in
-            </h1>
-            <div className="flex items-center flex-col space-y-4">
-                <button onClick={handleGoogleSignIn} className="group bg-gray-100 min-w-full h-12 px-6 border-2  border-none rounded-lg transition duration-300 hover:bg-black active:bg-black">
-                    <div className="relative flex items-center justify-center gap-4 w-full">
-                        <img className="block absolute left-0 w-5" src="https://tailus.io/sources/blocks/social/preview/images/google.svg" alt="google logo" />
-                        <span className="block font-medium tracking-wide text-black text-sm transition duration-300 group-hover:text-white sm:text-base">Sign in with Google</span>
-                    </div>
-                </button>
+  return (
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Sign in
+        </h1>
+        <div className="flex items-center flex-col space-y-4">
+          <button
+            onClick={handleGoogleSignIn}
+            className="group bg-gray-100 min-w-full h-12 px-6 border-2  border-none rounded-lg transition duration-300 hover:bg-black active:bg-black"
+          >
+            <div className="relative flex items-center justify-center gap-4 w-full">
+              <img
+                className="block absolute left-0 w-5"
+                src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
+                alt="google logo"
+              />
+              <span className="block font-medium tracking-wide text-black text-sm transition duration-300 group-hover:text-white sm:text-base">
+                Sign in with Google
+              </span>
             </div>
-
-            <form className="space-y-4 md:space-y-6" onSubmit={handleSignIn}>
-                <InputField
-                    text="Email"
-                    type="email"
-                    placeholder="example@example.com"
-                    id="email"
-                    autoComplete="true"
-                    required={true}
-                    value={userCreds.email}
-                    onChange={onChangeHandler}
-                />
-                <InputField
-                    id="password"
-                    text="Password"
-                    type="password"
-                    required={true}
-                    placeholder="••••••••"
-                    autoComplete="true"
-                    value={userCreds.password}
-                    onChange={onChangeHandler}
-                />
-
-                <button type="submit" className="w-full text-white  bg-gray-900 hover:bg-primary-700 hover:bg-black focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                <button onClick={openRegisterHandler} className="text-sm hover:underline font-light text-gray-500 dark:text-gray-400">
-                    Don't have an account?
-                </button>
-            </form>
+          </button>
         </div>
+
+        <form className="space-y-4 md:space-y-6" onSubmit={handleSignIn}>
+          <InputField
+            text="Email"
+            type="email"
+            placeholder="example@example.com"
+            id="email"
+            autoComplete="true"
+            required={true}
+            value={userCreds.email}
+            onChange={onChangeHandler}
+          />
+          <InputField
+            id="password"
+            text="Password"
+            type="password"
+            required={true}
+            placeholder="••••••••"
+            autoComplete="true"
+            value={userCreds.password}
+            onChange={onChangeHandler}
+          />
+
+          <button
+            type="submit"
+            className="w-full text-white  bg-gray-900 hover:bg-primary-700 hover:bg-black focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Create an account
+          </button>
+          <button
+            onClick={openRegisterHandler}
+            className="text-sm hover:underline font-light text-gray-500 dark:text-gray-400"
+          >
+            {`Don't`} have an account?
+          </button>
+        </form>
+      </div>
     </div>
+  );
 }
