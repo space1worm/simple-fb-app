8595f033c58062f6176f39154d35a5d1ebfa0946
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-01-17 22:38:13 +0400

    added yup&react-hook-form validation with spinner button

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,34 +1,56 @@
-import { ChangeEvent, HTMLInputTypeAttribute } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+import { CheckIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import { HTMLInputTypeAttribute } from "react";
 
 interface Props {
-    id: string;
-    text: string;
-    required: boolean;
-    type: HTMLInputTypeAttribute;
-    autoComplete?: string;
-    placeholder?: string;
-    value: string;
-    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  id: string;
+  title: string;
+  type: HTMLInputTypeAttribute;
+  autoComplete?: string;
+  placeholder?: string;
+  register: UseFormRegisterReturn;
+  errorMsg: string | undefined;
+  isDirty: boolean | undefined;
 }
 
-export default function InputField(props: Props) {
-    const { id, text, required, type, autoComplete, placeholder, value, onChange } = props;
-
-    return <div className="relative">
-        <label
-            htmlFor={id}
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            {text}
-        </label>
+export default function InputField({
+  id,
+  title,
+  autoComplete,
+  placeholder,
+  register,
+  errorMsg,
+  isDirty,
+  type,
+}: Props) {
+  return (
+    <div className="relative">
+      <label
+        htmlFor={id}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        {title}
+      </label>
+      <div className="flex justify-between items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <input
-            type={type}
-            autoComplete={autoComplete}
-            id={id}
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder={placeholder}
-            required={required}
-            value={value}
-            onChange={onChange}
+          id={id}
+          {...register}
+          type={type}
+          autoComplete={autoComplete}
+          className="flex-1 bg-transparent outline-none"
+          placeholder={placeholder}
         />
+        {!isDirty ? null : errorMsg ? (
+          <XCircleIcon className="h-4 text-red-500" />
+        ) : (
+          <CheckIcon className="h-4 text-green-500" />
+        )}
+      </div>
+      {isDirty && errorMsg ? (
+        <p className="text-ms text-red-500">{errorMsg}</p>
+      ) : (
+        <p className="text-ms text-transparent">no msg</p>
+      )}
     </div>
+  );
 }
