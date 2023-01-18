108aaa6c7b4088b9d590536f1e447e534e46d1e3
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-01-18 18:23:56 +0400

    test

--- lib/posts.ts
+++ lib/posts.ts
@@ -5,7 +5,7 @@ import { firebaseDB, firebaseStorage } from "../firebase";
 import { IPosts } from "../types/db/db.interface";
 import { TImgFile } from "../types/db/db.types";
 
-console.log("3233");
+console.log("32323");
 
 export const createPostImage = async (docId: string, file: TImgFile) => {
   const storageRef = ref(firebaseStorage, `posts/${docId}`);
