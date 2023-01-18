b01b7f83820894c78889514ba9aa974be73c44fd
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-01-18 16:26:48 +0400

    test

--- lib/authentication.ts
+++ lib/authentication.ts
@@ -1,9 +1,9 @@
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    GoogleAuthProvider,
-    signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 
 import { firebaseAuth } from "../firebase";
@@ -11,35 +11,35 @@ import { firebaseAuth } from "../firebase";
 const Gprovider = new GoogleAuthProvider();
 
 export const signUp = async (email: string, password: string) => {
-    return await createUserWithEmailAndPassword(firebaseAuth, email, password);
+  return await createUserWithEmailAndPassword(firebaseAuth, email, password);
 };
 
 export const logIn = async (email: string, password: string) => {
-    return await signInWithEmailAndPassword(firebaseAuth, email, password);
+  return await signInWithEmailAndPassword(firebaseAuth, email, password);
 };
 
 export const logOut = async () => {
-    await signOut(firebaseAuth);
+  await signOut(firebaseAuth);
 };
 
 export const signInWithGooglePopup = async () => {
-    try {
-        const result = await signInWithPopup(firebaseAuth, Gprovider);
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        const user = result.user;
-        return { user, token };
-    } catch (error: any) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
+  try {
+    const result = await signInWithPopup(firebaseAuth, Gprovider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+    const user = result.user;
+    return { user, token };
+  } catch (error: any) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
 
-        return {
-            errorCode,
-            errorMessage,
-            email,
-            credential,
-        };
-    }
+    return {
+      errorCode,
+      errorMessage,
+      email,
+      credential,
+    };
+  }
 };
